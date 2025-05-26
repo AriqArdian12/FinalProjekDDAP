2563213a850148ad8491657b628d7a710073e22a
Shunraii <artalarik2017@gmail.com>
2025-05-26 11:30:02 +0700

    Update Script.js

--- Js/Script.js
+++ Js/Script.js
@@ -68,3 +68,15 @@ document.addEventListener('DOMContentLoaded', () => {
         updateCarousel();
     }, 8000);
 });
+
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
